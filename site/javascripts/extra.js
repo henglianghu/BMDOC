8bfb08651ad5024fb1e2f7d8865399b4b663832d
LiuShuangWu <swliu@bigmath.com>
2024-10-28 15:17:25 +0800

    change: 第一版打包

--- site/javascripts/extra.js
+++ site/javascripts/extra.js
@@ -6,9 +6,9 @@
 //   s.parentNode.insertBefore(hm, s);
 //   console.log('百度统计代码已加载');
 // })();
-const navBar = document.getElementsByClassName('md-nav__title');
-Array.from(navBar).map((item) => {
-  if(item.getAttribute('for') === "__toc") {
-    item.innerHTML = "章节目录";
-  }
-});
+// const navBar = document.getElementsByClassName('md-nav__title');
+// Array.from(navBar).map((item) => {
+//   if(item.getAttribute('for') === "__toc") {
+//     item.innerHTML = "章节目录";
+//   }
+// });
