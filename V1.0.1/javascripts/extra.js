5476c7dc709bf2073f9609c1942678be42b327f7
LiuShuangWu <swliu@bigmath.com>
2024-10-31 17:35:45 +0800

    Deployed d8ef956 to V1.0.1 with MkDocs 1.5.3 and mike 2.1.3

---
+++ V1.0.1/javascripts/extra.js
@@ -0,0 +1,14 @@
+// var _hmt = _hmt || [];
+// (function () {
+//   var hm = document.createElement('script');
+//   hm.src = 'https://hm.baidu.com/hm.js?5b7b3f6f6d3c71ccfb2011dba89bac2f';
+//   var s = document.getElementsByTagName('script')[0];
+//   s.parentNode.insertBefore(hm, s);
+//   console.log('百度统计代码已加载');
+// })();
+// const navBar = document.getElementsByClassName('md-nav__title');
+// Array.from(navBar).map((item) => {
+//   if(item.getAttribute('for') === "__toc") {
+//     item.innerHTML = "章节目录";
+//   }
+// });
